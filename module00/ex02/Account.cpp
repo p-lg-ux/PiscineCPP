5c59fa6430beb97a9beb3ec47d4ba56ca1771ce3
Perrine GROS <pgros@e2r10p19.clusters.42paris.fr>
2023-03-22 22:22:22 +0100

    finished mod00

--- module00/ex02/Account.cpp
+++ module00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: pgros <pgros@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 11:54:59 by pgros             #+#    #+#             */
-/*   Updated: 2023/03/21 13:28:43 by pgros            ###   ########.fr       */
+/*   Updated: 2023/03/22 20:09:54 by pgros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <iostream>
 #include <string>
 #include <ctime>
+#include <iomanip>
 
 //constructors and destructors
 // Account::Account()
@@ -26,12 +27,52 @@
 // 	//std::cout << "Copy constructor of Account called." << std::endl;
 // }
 
+int	Account::getNbAccounts( void )
+{
+	return (Account::_nbAccounts);
+}
+
+int	Account::getTotalAmount( void )
+{
+	return (Account::_totalAmount);
+}
+
+int	Account::getNbDeposits( void )
+{
+	return (Account::_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals( void )
+{
+	return (Account::_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos( void )
+{
+	Account::_displayTimestamp();
+	std::cout << "accounts:" << _nbAccounts << ";";
+	std::cout << "total:" << _totalAmount << ";";
+	std::cout << "deposits:" << _totalNbDeposits << ";";
+	std::cout << "withdrawals:" << _totalNbWithdrawals << std::endl;
+}
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
 void	Account::_displayTimestamp( void )
 {
 	time_t	t = time(NULL);
 	struct tm *tm = localtime(&t);
 	 
-	std::cout << "[" << 1900 + tm->tm_year << "] ";
+	std::cout << "[" << 1900 + tm->tm_year;
+	std::cout << std::setw(2) << std::setfill('0') << tm->tm_mon + 1;
+	std::cout << std::setw(2) << std::setfill('0') << tm->tm_mday;
+	std::cout << "_" << std::setw(2) << std::setfill('0') << tm->tm_hour;
+	std::cout << std::setw(2) << std::setfill('0') << tm->tm_min;
+	std::cout << std::setw(2) << std::setfill('0') << tm->tm_sec;
+	std::cout << "] ";
 }
 
 Account::Account(int initial_deposit) : _amount(initial_deposit)
@@ -46,9 +87,64 @@ Account::Account(int initial_deposit) : _amount(initial_deposit)
 	std::cout << "amount:" << _amount << ";" ;
 	std::cout << "created" << std::endl ;
 }
+
 Account::~Account()
 {
-	//std::cout << "Destructor of Account called." << std::endl;
+	this->_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "amount:" << _amount << ";" ;
+	std::cout << "closed" << std::endl ;
+}
+
+void	Account::makeDeposit( int deposit )
+{
+	this->_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "p_amount:" << _amount << ";";
+	std::cout << "deposit:" << deposit << ";";
+	this->_amount += deposit;
+	this->_nbDeposits += 1;
+	_totalAmount += deposit;
+	_totalNbDeposits += 1;
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "nb_deposits:" << _nbDeposits << std::endl;
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	this->_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "p_amount:" << _amount << ";";
+	this->_amount -= withdrawal;
+	if (checkAmount())
+	{
+		this->_nbWithdrawals += 1;
+		_totalAmount -= withdrawal;
+		_totalNbWithdrawals += 1;
+		std::cout << "withdrawal:" << withdrawal << ";";
+		std::cout << "amount:" << _amount << ";";
+		std::cout << "nb_withdrawals:" << _nbWithdrawals << std::endl;
+		return (true);
+	}
+	this->_amount += withdrawal;
+	std::cout << "withdrawal:refused" << std::endl;
+	return (false);
+}
+
+int		Account::checkAmount( void ) const
+{
+	if (_amount < 0)
+		return (0);
+	return (1);
+}
+
+void	Account::displayStatus( void ) const
+{
+	this->_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "amount:" << _amount << ";" ;
+	std::cout << "deposits:" << _nbDeposits << ";" ;
+	std::cout << "withdrawals:" << _nbWithdrawals << std::endl ;
 }
 //overloads
 // Account& Account::operator=(const Account& rhs)
