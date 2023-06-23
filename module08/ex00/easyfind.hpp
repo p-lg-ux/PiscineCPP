a78d47dd7d139361bb1c2c862eb27670debb4029
Perrine GROS <pgros@bess-f3r1s1.clusters.42paris.fr>
2023-06-23 19:45:16 +0200

    started module08

---
+++ module08/ex00/easyfind.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pgros <pgros@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/23 19:21:26 by pgros             #+#    #+#             */
+/*   Updated: 2023/06/23 19:40:34 by pgros            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+# include <iostream>
+# include <string>
+
+template<typename T>
+T::iterator easyfind(T contain, int n)
+{
+	std::find();
+	return();
+}
+
+#endif
