7849249fdf8afa7dbfea6d4a6c04e5748ce4e0d0
Perrine GROS <pgros@paul-f4Ar5s1.clusters.42paris.fr>
2023-06-30 20:12:08 +0200

    cpp09/ex00: almost finished checkDate

--- module09/ex00/Input.cpp
+++ module09/ex00/Input.cpp
@@ -6,13 +6,15 @@
 /*   By: pgros <pgros@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 19:27:36 by pgros             #+#    #+#             */
-/*   Updated: 2023/06/29 21:38:34 by pgros            ###   ########.fr       */
+/*   Updated: 2023/06/30 20:09:51 by pgros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Input.hpp"
 #include <iostream>
+#include <iomanip>
 #include <string>
+#include <locale>
 
 //constructors and destructors
 Input::Input()
@@ -88,33 +90,73 @@ bool Input::splitLine()
 
 void Input::checkDateError()
 {
+	std::tm	time = {};
+	time_t	tmp_time; 
+	char	tmp_char[11];
+	std::string	tmp_str;
+
+	//TODO: trim whitespaces of _date
+	if (_date.length() != 10 || strptime(_date.c_str(), "%Y-%m-%d", &time) == NULL)
+	{
+		_errors |= BAD_INPT_DATE;
+		return;	
+	}
+	tmp_time = mktime(&time);
+	if (tmp_time < 0)
+	{
+		_errors |= BAD_INPT_DATE;
+		return;
+	}
+	strftime(tmp_char, 11, "%Y-%m-%d", &time);
+	tmp_str = tmp_char;
+	// std::cout << "-----------" << std::endl;
+	// std::cout << "original: " << _date << std::endl;
+	// std::cout << "after mktime: " << tmp_str << std::endl;
+	if (_date.find(tmp_str) == std::string::npos)
+		_errors |= BAD_INPT_DATE;
+	// if (iss.fail() || time.tm_year < 2009 || time.tm_year < 2022)
+	// 	_errors |= BAD_INPT_DATE;
+
 	//A MODIFIER AVEC LA STRUCTURE TIME
-	// std::istringstream	iss(_date);
 	// int					val;
 	// char				c;
+	// std::istringstream	iss(_date);
 	
-	// iss >> val;
-	// if (iss.fail() || val > 2022 || val < 2009)
+	// iss >> time.tm_year;
+	// if (iss.fail() || time.tm_year > 2022 || time.tm_year < 2009)
 	// 	_errors |= BAD_INPT_DATE;
 	// iss >> c;
-	// if (iss.fail() || c != 'c')
+	// if (iss.fail() || c != '-')
 	// 	_errors |= BAD_INPT_DATE;
-	// iss >> val;
-	// if (iss.fail() || val > 12 || val < 1)
+	// iss >> time.tm_mon;
+	// if (iss.fail() || time.tm_mon > 12 || time.tm_mon < 1)
 	// 	_errors |= BAD_INPT_DATE;
 	// iss >> c;
-	// if (iss.fail() || c != 'c')
+	// if (iss.fail() || c != '-')
 	// 	_errors |= BAD_INPT_DATE;
-	
+	// iss >> time.tm_mday;
+	// if (iss.fail() || time.tm_mday > 31 || time.tm_mday < 1)
+	// 	_errors |= BAD_INPT_DATE;
 }
 
-void	Input::checkYear();
-void	Input::checkMonth();
-void	Input::checkDay();
+// void	Input::checkYear();
+// void	Input::checkMonth();
+// void	Input::checkDay();
 
 void Input::checkValueError()
 {
-	// [...]
+	std::istringstream	iss(_value_str);
+
+	iss >> _value;
+	if (iss.fail())
+	{
+		_errors |= BAD_INPT_VALUE;
+		return;
+	}
+	if (_value < 0)
+		_errors |= NEG_VALUE;
+	if (_value > 1000)
+		_errors |= LARGE_VALUE;
 }
 
 Input& Input::operator=(std::string& line)
